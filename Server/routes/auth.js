3a69c0713b9a763d3965dc1e5a33b13f29775151
kunal649 <143793407+kunal649@users.noreply.github.com>
2025-03-02 23:48:58 +0530

    shift-core-logic-from-routeto-controller

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const passport = require("passport");
-const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
 const { verifyToken } = require("../config/jwt");
 const User = require("../models/user");
 const googleAuthenticator = require("../config/passport");
 const googleCallback = require("../config/passport");
 const userSignup = require("../controllers/user.controller");
+const userLogin = require("../routes/login");
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -18,22 +18,7 @@ router.get("/google/callback", googleCallback);
 
 router.post("/Signup", userSignup);
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = users.find((u) => u.email === email);
-
-  if (!user) return res.status(400).json({ message: "User not found" });
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
-
-  const token = jwt.sign({ id: user.id, email: user.email }, "SECRET_KEY", {
-    expiresIn: "1h",
-  });
-
-  res.cookie("token", token, { httpOnly: true, secure: false });
-  res.json({ user: { id: user.id, email: user.email }, token });
-});
+router.post("/login", userLogin);
 
 router.post("/logout", (req, res) => {
   res.clearCookie("token");
