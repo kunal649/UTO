9f4c614e8752528ba7bba308d2c685a04ba1507e
kunal649 <143793407+kunal649@users.noreply.github.com>
2025-03-13 23:33:04 +0530

    session-removed-> jwt-only-added

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -1,79 +1,56 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user.js");
-const { ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "http://localhost:5000/v1/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        const db = getDB();
         let user = await User.findOne({ email: profile.emails[0].value });
-
         if (!user) {
-          const user = await User.create                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   ({
+          user = new User({
             googleId: profile.id,
             name: profile.displayName,
             email: profile.emails[0].value,
             profilePic: profile.photos[0].value,
             password: null,
-            role: "user",
           });
-
-          user = await db
-            .collection("users")
-            .findOne({ _id: result.insertedId });
+          await user.save();
         }
-
-        done(null, user);
+        const token = jwt.sign(
+          { id: user._id, email: user.email },
+          process.env.JWT_SECRET,
+          { expiresIn: "2h" }
+        );
+        return done(null, { user, token });
       } catch (error) {
-        done(error, null);
+        return done(error, null);
       }
     }
   )
 );
-
-const googleAuthenticator = passport.authenticate("google", {
-  scope: ["profile", "email"],
-});
-const googleCallback = (req, res, next) => {
-  passport.authenticate("google", { failureRedirect: "/" }, (err, user) => {
-    if (err || !user) {
-      return res.redirect("/");
-    }
-
-    req.login(user, (loginErr) => {
-      if (loginErr) return next(loginErr);
-      return res.redirect("http://localhost:5143");
-    });
+const googleAuthenticator = (req, res, next) => {
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
   })(req, res, next);
 };
-
-// 🔹 Serialize user into session
-passport.serializeUser((user, done) => {
-  done(null, user._id.toString()); // Ensure `_id` is stored as a string
-});
-
-// 🔹 Deserialize user from session
-passport.deserializeUser(async (id, done) => {
-  try {
-    const db = getDB();
-    const user = await db
-      .collection("users")
-      .findOne({ _id: new ObjectId(id) });
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
-
+const googleCallback = (req, res, next) => {
+  passport.authenticate("google", { session: false }, (err, data) => {
+    if (err || !data) {
+      return res.redirect("http://localhost:5173");
+    }
+    res.cookie("token", data.token, { httpOnly: true, secure: false });
+    return res.redirect("http://localhost:5173");
+  })(req, res, next);
+};
 module.exports = {
-  passport,
   googleAuthenticator,
   googleCallback,
 };
