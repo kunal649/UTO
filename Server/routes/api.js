d78a4eadca8bdfc7b0ca5fc6cbcc2e56aeb046e3
kunal649 <143793407+kunal649@users.noreply.github.com>
2025-03-14 23:46:14 +0530

    update-04

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -2,8 +2,6 @@ const signRouter = require("./sign/sign.router.js");
 const loginRouter = require("./login/login.router.js");
 const express = require("express");
 
-const {verifyToken} = require('../middleware/jwt.js');
-
 const api = express.Router();
 
 api.use('/', signRouter);
