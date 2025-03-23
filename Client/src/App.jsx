dd92667648e8a6a0804945d7358c9145d27c5f96
kunal649 <143793407+kunal649@users.noreply.github.com>
2025-03-23 23:34:03 +0530

    leaflet+addons+css

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/hero/Home.jsx";
 import OurDoctors from "./pages/OurDoctors.jsx";
-import BlogComponent from "./pages/Discussion.jsx";
+//import BlogComponent from "./pages/Discussion.jsx";
 import Signup from "./pages/Auth/signup.jsx";
 import Login from "./pages/Auth/login.jsx";
+import ProfessionalNgoMap from "./pages/Map.jsx";
 
 import { AuthProvider } from "./context/Authcontext.jsx";
 import Navbar from "./components/navbar.jsx";
@@ -21,7 +22,6 @@ export default function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/discussion" element={<BlogComponent />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route
@@ -30,6 +30,7 @@ export default function App() {
                 <OurDoctors openAppointmentPopup={() => setIsPopupOpen(true)} />
               }
             />
+            <Route path="/Map" element={<ProfessionalNgoMap />} />
           </Routes>
           <AppointmentPopup
             isOpen={isPopupOpen}
