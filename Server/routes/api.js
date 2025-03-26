a180f8db6db499da3ff4d07c79817d7f6f95bfc0
kunal649 <143793407+kunal649@users.noreply.github.com>
2025-03-26 23:51:54 +0530

    discussion-router-connected

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,11 +1,12 @@
 const signRouter = require("./sign/sign.router.js");
 const loginRouter = require("./login/login.router.js");
+const discussionRouter = require("./discussion/discussion.controller.js");
 const express = require("express");
 
 const api = express.Router();
 
-api.use('/', signRouter);
-api.use('/', loginRouter);
-
+api.use("/", signRouter);
+api.use("/", loginRouter);
+api.use("/", discussionRouter);
 
 module.exports = api;
